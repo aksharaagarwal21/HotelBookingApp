fd62453255ec582e368a1df55de271ba188ceb36
aksharaagarwal21 <aksharakumari1208@gmail.com>
2026-03-13 15:14:52 +0530

    Base application setup

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,16 @@
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to Book My Stay ");
+        System.out.println(" Hotel Booking Management System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("======================================");
+
+        System.out.println("Application started successfully.");
+
+        // End of program
+        System.out.println("Thank you for using Book My Stay!");
+    }
+}
